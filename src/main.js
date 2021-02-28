29d48741ea350ebd1ba67c22ba4b37142c1bd403
Andrey Kolesnikov <mailbox.kolesnikov@mail.ru>
2021-02-28 13:43:17 +0300

    Добавил стили для шрифтов в element.ui

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from "./store";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 import locale from "element-ui/lib/locale/lang/ru-RU";
+import "element-ui/lib/theme-chalk/reset.css";
 
 Vue.config.productionTip = false;
 Vue.use(ElementUI, { locale });
