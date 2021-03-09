38de9d8d916c5453e1119dfdfdd5667028b672f7
Andrey Kolesnikov <mailbox.kolesnikov@mail.ru>
2021-03-09 23:30:09 +0300

    Сделал компонент шапки

---
+++ src/components/Header.vue
@@ -0,0 +1,98 @@
+<template>
+  <el-row class="header el-menu el-menu--horizontal">
+    <template v-if="!isLoading">
+      <div class="left-area">
+        <router-link
+          v-if="isAnonymous"
+          tag="h1"
+          :to="{ name: 'home' }"
+          class="logo"
+          >Todo
+        </router-link>
+      </div>
+      <ul class="menu">
+        <template v-if="isAnonymous">
+          <router-link
+            tag="li"
+            class="el-menu-item"
+            active-class="is-active"
+            :to="{ name: 'login' }"
+          >
+            Войти
+          </router-link>
+          <router-link
+            tag="li"
+            class="el-menu-item"
+            active-class="is-active"
+            :to="{ name: 'register' }"
+          >
+            Зарегистрироваться
+          </router-link>
+        </template>
+        <template v-if="isAuth">
+          <router-link
+            v-if="$route.name === 'project'"
+            tag="li"
+            class="el-menu-item"
+            active-class="is-active"
+            :to="{
+              name: 'createTask',
+              params: { projectId: this.$route.params.projectId }
+            }"
+            >Создать задачу</router-link
+          >
+          <li class="el-menu-item" @click="logout">Выйти</li>
+        </template>
+      </ul>
+    </template>
+  </el-row>
+</template>
+
+<script>
+import { getterTypes, actionTypes } from "@/store/auth";
+import { mapGetters } from "vuex";
+
+export default {
+  name: "Header",
+  computed: {
+    ...mapGetters({
+      isAuth: getterTypes.isAuth,
+      isAnonymous: getterTypes.isAnonymous,
+      isLoading: getterTypes.isLoading
+    })
+  },
+  methods: {
+    logout() {
+      this.$store
+        .dispatch(actionTypes.logout)
+        .then(() => {
+          if (this.$route.name !== "home") this.$router.push({ name: "home" });
+        })
+        .catch(() => {});
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.header {
+  min-height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  &::before,
+  &::after {
+    display: none;
+  }
+}
+
+.logo {
+  cursor: pointer;
+}
+
+.menu {
+  display: flex;
+  align-items: center;
+}
+</style>
