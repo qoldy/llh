d6a11f787d5f91ae6723d72d4c8784f76320e37a
Andrey Kolesnikov <mailbox.kolesnikov@mail.ru>
2021-03-11 21:21:00 +0300

    Добавил helper для даты

---
+++ src/helpers/date.js
@@ -0,0 +1,13 @@
+const convertToDatetime = dateInstance => {
+  let date = dateInstance
+    .toLocaleDateString()
+    .split(".")
+    .reverse()
+    .join("-");
+  let time = dateInstance.toLocaleTimeString();
+  return `${date} ${time}`;
+};
+
+export default {
+  convertToDatetime
+};
