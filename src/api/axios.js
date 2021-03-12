35ce02f95be166ba0ab46244beb98345f25b08e7
Andrey Kolesnikov <mailbox.kolesnikov@mail.ru>
2021-03-12 18:54:44 +0300

    Сделал вывод фатальной ошибки

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import token from "@/helpers/token";
+import ElementUI from "element-ui";
 
 const instance = axios.create({
   baseURL: "http://localhost:8080/api"
@@ -9,5 +10,13 @@ instance.interceptors.request.use(config => {
   config.headers["Authorization"] = `Bearer ${token.get()}`;
   return config;
 });
+
+instance.interceptors.response.use(
+  response => response,
+  error => {
+    if (error.response.status >= 500) ElementUI.Message.error("Ошибка сервера");
+    return Promise.reject(error);
+  }
+);
 
 export default instance;
